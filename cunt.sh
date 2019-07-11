3f1e45fb5a40c1c439d0a92770b486c6b26b44a2
UtsavisGreat <utsavbalar1231@gmail.com>
2019-07-11 15:19:18 +0000

    buildscript: use gcc-9.1 toolchain
    Signed-off-by: UtsavisGreat <utsavbalar1231@gmail.com>

--- cunt.sh
+++ cunt.sh
@@ -5,4 +5,4 @@ export KBUILD_COMPILER_STRING=UtsavTheCunt-CuntsSpace
 make sanders_defconfig O=out
 make -j8 O=out \
         CROSS_COMPILE_ARM32=~/arm/bin/arm-linux-androideabi- \
-        CROSS_COMPILE=~/gcc/bin/aarch64-linux-android- 2>&1 | tee error.log
+        CROSS_COMPILE=~/gcc-9/bin/aarch64-elf- 2>&1 | tee error.log
